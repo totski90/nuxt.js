9e9b840a3bf29cdc95eac75e8d0df8039ed87992
vic datu andam <vica@fligno.com.ph>
2019-02-19 15:35:49 +0800

    First commit

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,61 @@
+<template>	
+
+	<section class="single-recipe">
+		
+		<h1>{{ recipe.title }}</h1>
+
+		<div>
+			<img class="recipe-image" :src="recipe.thumbnail" alt="recipe.title">
+
+		</div>
+
+		<p>{{ recipe.descriptionText }}</p>
+
+	</section>
+
+</template>
+
+<script>
+
+export default {	
+	asyncData(context) {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve({
+						recipe: [
+							{
+								id: "1",
+								title: "Delicious food",
+								descriptionText: "This is really a description text",
+								thumbnail: "https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg"
+							},
+							{
+								id: "2",
+								title: "What a nice place",
+								descriptionText: "This is really a description text",
+								thumbnail: "https://images.pexels.com/photos/556416/pexels-photo-556416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+							}
+						].find(el => el.id === context.params.id)				
+					})
+				}, 1500)
+			})
+		}
+	}
+
+</script>
+<style scoped>
+	
+	.single-recipe {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		padding: 30px;
+		flex-flow: column;
+	}
+
+	.recipe-image {
+		width: 100%;		
+	}
+
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,61 @@
+<template>
+
+	<section class="recipes">
+
+		<Recipe	v-for="recipe in recipes"
+			:key="recipe.id"
+			:thumbnail="recipe.thumbnail"
+			:title="recipe.title"
+			:id="recipe.id"		
+			:previewText="recipe.previewText"	
+		/>
+
+	</section>
+
+</template>
+
+<script>
+	
+	import Recipe from '@/components/Recipe';
+
+	export default {
+		components: {
+			Recipe
+		},
+
+		asyncData() {
+			return new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve({
+						recipes: [
+							{
+								id: "1",
+								title: "Delicious food",
+								previewText: "Awesome food desert!",
+								thumbnail: "https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg"
+							},
+							{
+								id: "2",
+								title: "What a nice place",
+								previewText: "A beutiful place to live in!",
+								thumbnail: "https://images.pexels.com/photos/556416/pexels-photo-556416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+							}
+						]
+					})
+				}, 1500)
+			})
+		}
+	}
+
+</script>
+
+<style scoped>
+
+	.recipes    {
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		align-items: center;
+	}
+
+</style>
